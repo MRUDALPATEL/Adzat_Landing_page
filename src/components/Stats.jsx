e4d2612f179487a982ac5828ba72523f9de9fd2b
MRUDALPATEL <mrudalpatel5336@gmail.com>
2025-09-19 16:24:38 +0530

    stats changes

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -3,15 +3,18 @@ import styles from "../style";
 
 const Stats = () => {
   return (
-    <section className={`${styles.flexCenter} flex-row mb-10 `}>
+    <section
+      className={`${styles.flexCenter} flex flex-row flex-nowrap justify-center  mb-10 w-full`}
+    >
       {stats.map((stat, index) => (
         <div
           key={stat.id}
           className={`
             flex flex-col items-center justify-center 
-            w-[450px] h-[450px]  
-            rounded-full 
-            text-center relative border
+            rounded-full text-center relative border
+            flex-1 min-w-[120px] sm:min-w-[150px] md:min-w-[180px] lg:min-w-[200px] xl:min-w-[250px]
+            max-w-[450px]
+            aspect-square
             ${index === 0 ? "border-blue-500" : ""}
             ${index === 1 ? "border-green-500" : ""}
             ${index === 2 ? "border-orange-500" : ""}
@@ -26,12 +29,12 @@ const Stats = () => {
           }}
         >
           {/* Number */}
-          <h4 className="font-inter font-extrabold text-5xl text-white">
+          <h4 className="font-inter font-extrabold text-lg sm:text-2xl md:text-3xl lg:text-4xl text-white">
             {stat.value}
           </h4>
 
           {/* Title */}
-          <p className="mt-2 font-inter font-medium text-lg text-gray-200">
+          <p className="mt-2 font-inter font-medium text-xs sm:text-sm md:text-base lg:text-lg text-gray-200">
             {stat.title}
           </p>
         </div>
